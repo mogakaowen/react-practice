a045a54d1028d1f57cbccdc871c54e07f441df09
Owen Mogaka <owenmogaka@gmail.com>
2024-01-13 13:48:01 +0300

    added books

--- src/books.js
+++ src/books.js
@@ -1,6 +1,12 @@
 import img1 from "./images/fourth-wing.jpg";
 import img2 from "./images/curious-minds.jpg";
 import img3 from "./images/heaven-earth.jpg";
+import img4 from "./images/oath-and-honor.jpg";
+import img5 from "./images/flame-shadow.jpg";
+import img6 from "./images/miracle-morning.jpg";
+import img7 from "./images/housemaid.jpg";
+import img8 from "./images/first-lie.jpg";
+import img9 from "./images/bright-creatures.jpg";
 
 export const books = [
   {
@@ -20,5 +26,41 @@ export const books = [
     title: "The Heaven & Earth Grocery Store: A Novel",
     img: img3,
     author: "James McBride",
+  },
+  {
+    id: 4,
+    title: "Oath and Honor: A Memoir and a Warning",
+    img: img4,
+    author: "Liz Cheney",
+  },
+  {
+    id: 5,
+    title: "House of Flame and Shadow (Crescent City, 3)",
+    img: img5,
+    author: "Sarah J. Maas",
+  },
+  {
+    id: 6,
+    title: "The Miracle Morning (Updated Edition)",
+    img: img6,
+    author: "Hal Elrod",
+  },
+  {
+    id: 7,
+    title: "The Housemaid: A Novel of Suspense",
+    img: img7,
+    author: "Freida McFadden",
+  },
+  {
+    id: 8,
+    title: "First Lie Wins: Reese's Book Club Pick (A Novel)",
+    img: img8,
+    author: "Ashley Elston",
+  },
+  {
+    id: 9,
+    title: "Remarkably Bright Creatures (A Novel)",
+    img: img9,
+    author: "Shelby Van Pelt",
   },
 ];
